c2e016bcddec19d69a140e4d324012aaa02ac090
DavidGomez27 <mmcsgomez16@gmail.com>
2017-10-02 11:49:20 -0400

    adding new array

--- Week2_Day1Arrays/Program.cs
+++ Week2_Day1Arrays/Program.cs
@@ -10,6 +10,57 @@ namespace Week2_Day1Arrays
     {
         static void Main(string[] args)
         {
+
+
+            //pluralize since arrays are always groups of related information
+
+
+            //int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
+
+            //string[] studentNames = { "Raz", "Monica", "David", "Brian", "Ali" };
+
+            //Console.WriteLine(favoriteNumbers[0]);
+            //Console.WriteLine(studentNames[2]);
+
+
+            //int[] childrenAge = { 7, 12, 1 };
+            //Console.WriteLine(childrenAge[2]);
+            
+            //decimal[] gpaScore = { 1.2m, 2.3m, 1.8m, 4.0m, 3.2m, 3.8m, 2.9m, 3.9m };
+            //Console.WriteLine(gpaScore[6]);
+
+
+            //char[] firstLetter = { 'B', 'Y', 'K', 'F' };
+            //Console.WriteLine(firstLetter[0]);
+
+
+            //string[] vacaSpots = { "USA", "Germany", "Australia", "Canada", "Italy", "Egypt", "Madagascar", "Portugal" };
+            //Console.WriteLine(vacaSpots[4]);
+
+
+            //declaring an empty array
+            int[] numbers = new int[4];
+            numbers[0] = 43; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
