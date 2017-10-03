0d96d8880554b374da641dd442d611ed9e0c46c0
DavidGomez27 <mmcsgomez16@gmail.com>
2017-10-03 11:41:21 -0400

    finished practice

--- Week2_Day1Arrays/Program.cs
+++ Week2_Day1Arrays/Program.cs
@@ -89,10 +89,51 @@ namespace Week2_Day1Arrays
             //Console.WriteLine(example[indexMinusTwo]);
 
 
-            string[] example = { "This", "is", "an", "example" };
-            Console.WriteLine("This is the last index " + (example.Length - 1));
+            //string[] example = { "This", "is", "an", "example" };
+            //Console.WriteLine("This is the last index " + (example.Length - 1));
+
+            //Console.WriteLine(example[example.Length - 1]); //to get last element follow this way
 
 
+
+
+            //int arraySize = int.Parse(Console.ReadLine());
+            //int[] newArray = new int[arraySize];
+
+            //Console.WriteLine("The array is of length " + newArray.Length);
+
+
+
+            //string[] birthMonth = { "October", "July", "October", "May", };
+            //Console.WriteLine(Array.IndexOf(birthMonth,"October"));
+
+
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 3, 8, 1 };
+            //Console.WriteLine(Array.LastIndexOf(numbers, 3));
+
+
+            //Array.Reverse(numbers);
+            //Console.WriteLine(numbers[0]);
+
+
+            //Array.Sort(numbers);//sort the objects of the array
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[numbers.Length - 1]);
+
+
+            //Practice Problems
+            //string[] firstNames = { "David", "Nathan", "Kevin", "Brian", "Yusuf" };
+            //Console.WriteLine(Array.IndexOf(firstNames, "Kevin"));
+
+            //int[] luckyNumbers = { 12, 34, 32, 7, 34, 12, 7, 32 };
+            //Console.WriteLine(Array.LastIndexOf(luckyNumbers, 7));
+
+            
+            //char[] letters = { 'a', 'b', 'c', 'p', 'x', 'n', 'd' };
+            //Console.WriteLine(letters[0]);
+            //Array.Reverse(letters);
+            //Console.WriteLine(letters[0]);
 
 
 
